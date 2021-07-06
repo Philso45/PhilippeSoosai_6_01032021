489b1b3f6c1a4c579f9f794cb0a0b3c29180e2a2
Philippe Soosai <philippesoosai@gmail.com>
2021-07-06 12:01:14 +0200

    ajout de la fonction de validation des champs contre les injections

--- Backend/models/Sauce.js
+++ Backend/models/Sauce.js
@@ -1,13 +1,15 @@
 //Import de mongoose
 const mongoose = require('mongoose');
+// Appel du middleware de validation des champs du model de la sauce
+const sauceValidation = require('../middleware/sauceValidation');
 
 // Création du schéma de données Sauce qui contient les champs souhaité pour chaque sauce en indiquant leur caractère obligatoire ou non
 const sauceSchema = mongoose.Schema({
   userId: { type: String, required: true },
-  name: { type: String, required: true },
-  manufacturer: { type: String, required: true },
-  description: { type: String, required: true },
-  mainPepper: { type: String, required: true },
+  name: { type: String, required: true, validate: sauceValidation.nameValidator },
+  manufacturer: { type: String, required: true, validate: sauceValidation.manufacturerValidator },
+  description: { type: String, required: true, validate: sauceValidation.descriptionValidator },
+  mainPepper: { type: String, required: true, validate: sauceValidation.pepperValidator },
   imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
   likes: { type: Number, default: 0 },
